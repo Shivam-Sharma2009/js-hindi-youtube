76c07fdacff08b9e3007d6e4f9a00f8afd8f48ac
SHIVAM SHARMA <pgcshivamsharma@gmail.com>
2026-09-09 03:25:29 +0530

    Refactor conversionOperation.js for better readability
    Reorganize and uncomment code for clarity on type conversion operations.

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,10 +20,45 @@ let booleanIsLoggedIn = Boolean (isLoggedIn);
 
 // 1 => true;  0 => false
 // "" => false;
-//   "hitesh" => true
+//   "Shivam" => true
 
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let value = 3
+let NegValue = -value
+// console.log(NegValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Shivam"
+let str3 =  str1 + str2
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+ 2 + 2);
+// console.log( 1 + 2 + "2");
+// console.log( (3+4) * 5 % 3);
+
+// console.log(true);
+// console.log(+"");
+
+
+let num1 ,num2, num3 ;
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+// link https://tc39.es/ecma262
