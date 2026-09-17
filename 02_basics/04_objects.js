b21d7b344a1f3fdd61e60222aa059cd9c8077b69
SHIVAM SHARMA <pgcshivamsharma@gmail.com>
2026-09-17 15:21:18 +0530

    Add files via upload

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,55 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123ab"
+tinderUser.name = "sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname : {
+        userfullname: {
+            firstname: "Shivam" , 
+            Lastname: "Sharma"
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const Obj1 = {1: "a", 2: "b"}
+const Obj2 = {3: "c", 4: "d"}
+const Obj4 = {5: "c", 6: "d"}
+
+// const Obj3 = {Obj1, Obj2} 
+// const Obj3 = Object.assign( {}, Obj1 , Obj2 , Obj4) 
+
+const Obj3 = {...Obj1,...Obj2,...Obj4}
+
+console.log(Obj3);
+
+const user = [
+    {
+        id: "1" ,
+        email: "S@google.com"
+    },
+        {
+        id: "1" ,
+        email: "S@google.com"
+    },
+        {
+        id: "1" ,
+        email: "S@google.com"
+    }
+]
+
+user[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+console.log(tinderUser.hasOwnProperty('isLogged'));
