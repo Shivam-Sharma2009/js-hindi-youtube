f261c440749d01741bb17f4c5f5748a540beb51c
Shivam Sharma <pgcshivamsharma@gmail.com>
2026-09-21 12:19:00 +0530

    functions learning

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,36 @@
+
+function sayMyName(){
+    console.log("S");
+    console.log("h");
+    console.log("i");
+    console.log("v");
+    console.log("a");
+    console.log("m");
+}
+
+// sayMyName()
+
+// function addTwoNumber(number1,number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumber(number1,number2){   
+    // let result = number1 + number2
+    // return result 
+    return number1 + number2
+}
+
+const result = addTwoNumber(4,5)
+
+// console.log("Result:",result);
+
+function loginUserMessage(username = "sam"){
+    if (!username){
+        console.log("Please Enter Your UserName");
+        return        
+    }
+    return `${username} just logged In`
+}
+
+// console.log(loginUserMessage("Shivam"));
+console.log(loginUserMessage("Shivam"));
